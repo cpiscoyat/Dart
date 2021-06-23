3d0db81e3aca43abdcab0df774fe6b56409061a3
Carlos Piscoya <cpiscoyat@gmail.com>
2021-06-23 17:32:42 -0500

    Create dos.dart

---
+++ dos.dart
@@ -0,0 +1,34 @@
+void main() {
+  int a = 12;
+  int b = 5;
+  print("***Operadores Aritméticos***");
+
+  print("La suma es ${a + b}");
+  print("La resta es ${a - b}");
+  print("La multiplicación es ${a * b}");
+  print("La división es ${a / b}");
+  print("La división entera es ${a ~/ b}");
+  print("El resto es ${a % b}");
+
+  print("***Operadores de Incremento y Decremento***");
+  print("La variable a incrementa en 1 ${a++} "); //12
+  print("La variable a incrementa en 1 ${++a} "); //14
+  print("La variable a incrementa en 1 ${b--} "); //5
+  print("La variable a incrementa en 1 ${--b} "); //3
+
+  print("***Operadores de Asignación***");
+  a += b; //17
+  b -= 2; //1
+  print("La variable a es igual a $a");
+  print("La variable b es igual a $b");
+  a *= 3; //51
+  print("La variable a es igual a $a");
+  a %= 2; //1
+  print("La variable a es igual a $a");
+
+  print("***Operadores de Comparación y Relación***");
+  print("a es igual a b: ${a == b}");
+  print("a es diferente a b: ${a != b}");
+
+
+}
